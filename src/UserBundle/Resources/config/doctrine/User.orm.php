a5fa5f5df30c365e1de1a9b1fb5eab684eae0fee
tekayamehdi <mehdi.tekaya@esprit.tn>
2020-02-10 16:30:32 +0100

    Initial commit

---
+++ src/UserBundle/Resources/config/doctrine/User.orm.php
@@ -0,0 +1,14 @@
+<?php
+
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+$metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_NONE);
+$metadata->customRepositoryClassName = 'UserBundle\Repository\UserRepository';
+$metadata->setChangeTrackingPolicy(ClassMetadataInfo::CHANGETRACKING_DEFERRED_IMPLICIT);
+$metadata->mapField(array(
+   'fieldName' => 'id',
+   'type' => 'integer',
+   'id' => true,
+   'columnName' => 'id',
+  ));
+$metadata->setIdGeneratorType(ClassMetadataInfo::GENERATOR_TYPE_AUTO);
